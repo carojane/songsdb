6d3f37c6274ce9f0d65d884ce0d0a4d421e82031
carojane <carojane@gmail.com>
2014-09-10 13:29:31 -0400

    linked artist_id to songs, remove artist field, change Prince name

--- db/migrate/20140910171112_create_artists.rb
+++ db/migrate/20140910171112_create_artists.rb
@@ -1,4 +1,11 @@
 class CreateArtists < ActiveRecord::Migration
   def change
+    create_table :artists do |t|
+      t.string :name, null: false
+
+      t.timestamps
+    end
+
+    add_column :songs, :artist_id, :integer
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,18 +11,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140910165015) do
+ActiveRecord::Schema.define(version: 20140910172311) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "artists", force: true do |t|
+    t.string   "name",       null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "songs", force: true do |t|
     t.string   "title",      null: false
     t.string   "album",      null: false
-    t.string   "artist",     null: false
     t.integer  "year",       null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "artist_id"
   end
 
 end
